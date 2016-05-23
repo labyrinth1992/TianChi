c947e053edd198b5e2dd91efe3b92f4923b85b61
bxq <wrongtest0@gmail.com>
2016-05-23 20:04:46 +0800

    喵喵

--- feature/per_song/fs_last_month.py
+++ feature/per_song/fs_last_month.py
@@ -1,5 +1,5 @@
 from feature.feature_set import SQLFeatureSet
-
+from io.sql import SQLClient
 
 class FsSongLastMonthBasicStatistics(SQLFeatureSet):
 
@@ -9,10 +9,16 @@ class FsSongLastMonthBasicStatistics(SQLFeatureSet):
     def __init__(self):
         SQLFeatureSet.__init__(self, "song_last_month_basic_statistics", self.features(), "song_id")
 
-    def make(self, tables):
+    def make(self, params):
+        output_name = "fs_" + self.name
         sql = """
-        SELECT song_id, count(if(action=0, 1, 0)), count(if(action=1, 1, 0)), count(if(action=2, 1, 0))
+        CREATE TABLE %s AS
+        SELECT song_id, count(if(action=0, 1, 0)) AS last_month_play,
+                        count(if(action=1, 1, 0)) AS last_month_download,
+                        count(if(action=2, 1, 0)) AS last_month_collect
         FROM %s
-        GROUP BY song_id 
-        WHERE time > 0
-       """ % tables["src"]
+        GROUP BY song_id
+        WHERE ds > 0 AND ds < 0
+       """ % (output_name, params["src"])
+        SQLClient.execute(sql)
+        return output_name
