aef32286959fd22daa7df443a5ad4b1aabee3216
bxq <bxq@wrongtest>
2016-05-31 20:51:01 +0800

    iiii

--- models/simple_per_song_model.py
+++ models/simple_per_song_model.py
@@ -3,6 +3,7 @@ from models.model import Model
 from utils.sql import SQLClient
 from sklearn.ensemble import RandomForestRegressor
 
+
 class SimplePerSongModel(Model):
 
     def __init__(self, feature_set, predict_table_name):
@@ -19,14 +20,14 @@ class SimplePerSongModel(Model):
         self.feature_set.make(params)
         sql = """
         select * from %s
-        """ %params["table"]
+        """ % self.feature_set.name
         data = SQLClient.execute(sql)
         self.x = [_[1:-1] for _ in data]
         self.y = [_[-1] for _ in data]
 
     def train(self):
         self.x = numpy.asarray(self.x)
-        self.clf = RandomForestRegressor(n_estimator=100, min_samples_leaf=2)
+        self.clf = RandomForestRegressor(n_estimators=100, min_samples_leaf=2)
         self.clf.fit(self.x, self.y)
 
     def make_predict_data(self, params):
