43b15d1cbaff5bbf7f5f4479f79a4699d387f6ed
yxwang <yxwang@mobvoi.com>
2016-06-01 14:57:42 +0800

    miao

--- models/simple_per_song_model.py
+++ models/simple_per_song_model.py
@@ -2,7 +2,9 @@ import numpy
 from models.model import Model
 from utils.sql import SQLClient
 from sklearn.ensemble import RandomForestRegressor
-
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.externals import joblib
+from sklearn.metrics import mean_squared_error
 
 class SimplePerSongModel(Model):
 
@@ -15,6 +17,7 @@ class SimplePerSongModel(Model):
         self.test_x = None
         self.test_y = None
         self.predict_res = None
+        self.vectorizer = None
 
     def make_train_data(self, params):
         sql = "select * from " + self.feature_set.make(params, output_table=self.feature_set.name + "_train")
@@ -23,9 +26,11 @@ class SimplePerSongModel(Model):
         self.y = [_[-1] for _ in data]
 
     def train(self):
-        self.x = numpy.asarray(self.x)
+        self.vectorizer = DictVectorizer()
+        self.x = self.vectorizer.fit_transform(x_train)
         self.clf = RandomForestRegressor(n_estimators=100, min_samples_leaf=2)
         self.clf.fit(self.x, self.y)
+        save_model()
 
     def make_predict_data(self, params):
         sql = "select * from " + self.feature_set.make(params, output_table=self.feature_set.name + "_predict")
@@ -34,7 +39,16 @@ class SimplePerSongModel(Model):
         self.test_y = [_[-1] for _ in data]
 
     def predict(self):
-        test_x = self.clf.fit_transform(self.test_x)
+        load_model()
+        self.test_x = self.vectorizer.transform(self.test_x)
         self.predict_res = self.clf.predict(test_x)
-        print self.predict_res
+        print mean_squared_error(test_x, self.predict_res)
+
+
+    def save_model(self):
+        joblib.dump(self.clf, "train_model.rf")
+
+    def load_model(self):
+        self.clf = joblib,load("train_model.rf")
+
 
